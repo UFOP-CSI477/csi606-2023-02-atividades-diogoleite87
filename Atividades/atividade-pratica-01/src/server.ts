dbe356304c8ec3d1c38ad656579ee95d73f821f7
Diogo Leite <diogoleite87@gmail.com>
2023-11-13 19:30:56 -0300

    feat: add cors config

--- Atividades/atividade-pratica-01/src/server.ts
+++ Atividades/atividade-pratica-01/src/server.ts
@@ -5,8 +5,13 @@ import { personRoutes } from "./routes/person.routes";
 import { stateRoutes } from "./routes/state.routes";
 import fastify, { FastifyInstance } from "fastify";
 import { cityRoutes } from "./routes/city.routes";
+import cors from '@fastify/cors'
 
 const app: FastifyInstance = fastify();
+
+await app.register(cors, {
+    origin: '*'
+})
 
 app.register(personRoutes, {
     prefix: '/person'
