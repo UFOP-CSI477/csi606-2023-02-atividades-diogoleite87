6141c818279f1c8f1d8dab54fe6a2cbd7fc7dbf1
Diogo Leite <diogoleite87@gmail.com>
2024-01-13 15:28:01 -0300

    feat: add user dto schema

--- Projeto/frontend/src/router/router.tsx
+++ Projeto/frontend/src/router/router.tsx
@@ -1,7 +1,10 @@
+import Profile from "../pages/Profile"
 import Login from "../pages/Login"
 import Home from "../pages/Home"
+import User from "../pages/User"
 
 import { Route, Routes } from "react-router-dom"
+import { AuthAdminRoutes } from "./admin.routes"
 import { AuthRoutes } from "./auth.routes"
 
 export function Router() {
@@ -13,6 +16,16 @@ export function Router() {
                 <AuthRoutes>
                     <Home />
                 </AuthRoutes>
+            } />
+            <Route path="/profile" element={
+                <AuthRoutes>
+                    <Profile />
+                </AuthRoutes>
+            } />
+            <Route path="/user" element={
+                <AuthAdminRoutes>
+                    <User />
+                </AuthAdminRoutes>
             } />
         </Routes>
     )
--- Projeto/frontend/src/schemas/dto.ts
+++ Projeto/frontend/src/schemas/dto.ts
@@ -2,3 +2,10 @@ export type LoginDTO = {
     email: string,
     password: string
 }
+
+export type UserDTO = {
+    name: string,
+    description: string,
+    password: string,
+    email: string
+}
