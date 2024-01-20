633d00a544c78d775bdad4c20b15f5f22cff06ca
Diogo Leite <diogoleite87@gmail.com>
2024-01-19 21:43:31 -0300

    feat: customer page

--- Projeto/frontend/src/router/router.tsx
+++ Projeto/frontend/src/router/router.tsx
@@ -1,5 +1,10 @@
+import Category from "../pages/Category"
+import Customer from "../pages/Customer"
 import Profile from "../pages/Profile"
+import Orders from "../pages/Orders"
 import Login from "../pages/Login"
+import Order from "../pages/Order"
+import Item from "../pages/Item"
 import Home from "../pages/Home"
 import User from "../pages/User"
 
@@ -26,6 +31,32 @@ export function Router() {
                 <AuthAdminRoutes>
                     <User />
                 </AuthAdminRoutes>
+            } />
+            <Route path="/customer" element={
+                <AuthRoutes>
+                    <Customer />
+                </AuthRoutes>
+            } />
+
+            <Route path="/category" element={
+                <AuthRoutes>
+                    <Category />
+                </AuthRoutes>
+            } />
+            <Route path="/item" element={
+                <AuthRoutes>
+                    <Item />
+                </AuthRoutes>
+            } />
+            <Route path="/order" element={
+                <AuthRoutes>
+                    <Orders />
+                </AuthRoutes>
+            } />
+            <Route path="/order/:id" element={
+                <AuthRoutes>
+                    <Order />
+                </AuthRoutes>
             } />
         </Routes>
     )
