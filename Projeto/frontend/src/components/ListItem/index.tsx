7e3025ccd30a346606e00410f7cc444e53288bba
Diogo Leite <diogoleite87@gmail.com>
2024-01-20 22:38:04 -0300

    feat: order CRUD

--- Projeto/frontend/src/components/ListItem/index.tsx
+++ Projeto/frontend/src/components/ListItem/index.tsx
@@ -1,6 +1,7 @@
+import { Category, Home, Inventory, ListAlt, People, SupervisedUserCircle } from '@mui/icons-material';
 import { List, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material'
 import { useNavigate } from 'react-router-dom';
-import { Home } from '@mui/icons-material';
+import { useAuth } from '../../hooks/useAuth';
 
 interface MyListItemProps {
     icon: JSX.Element
@@ -26,9 +27,19 @@ function MyListItem({ icon, router, iconText }: MyListItemProps) {
 
 export function DrawerListItem() {
 
+    const { authData } = useAuth()
+
     return (
         <List>
             <MyListItem icon={<Home />} router='/' iconText="Início" />
+            {
+                authData?.user.type == 1 && <MyListItem icon={<SupervisedUserCircle />} router='/user' iconText="Funcionários" />
+            }
+            <MyListItem icon={<People />} router='/customer' iconText="Clientes" />
+            <MyListItem icon={<Category />} router='/category' iconText="Categorias" />
+            <MyListItem icon={<Inventory />} router='/item' iconText="Itens" />
+            <MyListItem icon={<ListAlt />} router='/order' iconText="Pedidos" />
+
         </List>
     )
 }
